75c3bfb59a3b304daff1a7d88fb6fe43526687f5
ZuzRad <zuzarad785@student.polsl.pl>
2023-08-10 18:52:03 +0200

    Trampoline update

--- Assets/Scripts/Traps/Trampoline.cs
+++ Assets/Scripts/Traps/Trampoline.cs
@@ -4,33 +4,23 @@ using UnityEngine;
 
 public class Trampoline : MonoBehaviour
 {
-	[SerializeField] private float blowPower = 16f;
-	[SerializeField] private float forceInterval = 0.1f;
+	[SerializeField] private float forcePower = 16f;
 
 	private Rigidbody2D rb;
 	private float timeSinceLastBlow = 0f;
-	private void OnTriggerStay2D(Collider2D collision)
-	{
-		timeSinceLastBlow += Time.deltaTime;
+	private Animator animator;
 
-		if (timeSinceLastBlow >= forceInterval)
-		{
-			rb.AddForce(Vector2.up * blowPower, ForceMode2D.Impulse);
-			timeSinceLastBlow = 0f;
-		}
-	}
-	private void OnTriggerEnter2D(Collider2D collision)
+    private void Start()
+    {
+		animator = GetComponent<Animator>();
+    }
+    private void OnTriggerStay2D(Collider2D collision)
 	{
 		if (collision.TryGetComponent(out Movement player))
 		{
-			rb = collision.GetComponent<Rigidbody2D>();
-		}
-	}
-	private void OnTriggerExit2D(Collider2D collision)
-	{
-		if (collision.TryGetComponent(out Movement player))
-		{
-			rb = null;
+			rb = player.GetComponent<Rigidbody2D>();
+			rb.AddForce(Vector2.up * forcePower, ForceMode2D.Impulse);
+			animator.SetTrigger("standing");
 		}
 	}
 }
